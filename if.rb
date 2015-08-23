a0b62dd789aa92c54b581c7a1bf0629e344af45b
alnugen <lim.nugen@gmail.com>
2015-08-23 23:54:54 +0545

    Create if.rb

---
+++ if.rb
@@ -0,0 +1,19 @@
+a='alis'
+
+if a=='alis'
+	puts "hi"
+end 
+
+if a!='alis'
+	puts "hi"
+else 
+	puts "hey"
+end
+
+if a!='alis'
+	puts 'hi'
+elsif a=='nugen'
+	puts 'hola'
+else
+	a="Nevermind"
+end
